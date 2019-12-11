9b5805c172d930879a2b5b7a5e1ab2cdf119e93b
Daniel Gallagher <daniel.gallagher@elastic.co>
2019-12-11 13:31:30 -0500

    Added existing cert check
    Added existing Letsencrypt cert check from upstream branch.

--- certbot-nginx/start.sh
+++ certbot-nginx/start.sh
@@ -7,15 +7,22 @@ echo  "----------------------------------------------------------------"
 
 sed -i "s/___my.example.com___/$MY_DOMAIN_NAME/g" /etc/nginx/nginx.conf
 
+openssl x509 -in /etc/letsencrypt/live/$MY_DOMAIN_NAME/cert.pem -checkend 604800 > /dev/null 2>&1
+
 # Enable staging mode if needed
 if [ $STAGING != "0" ]; then staging_arg="--staging"; fi
 
-nginx
-sleep 5
-
-certbot $staging_arg --nginx -d $MY_DOMAIN_NAME -d www.$MY_DOMAIN_NAME --text --agree-tos --email $EMAIL_ADDRESS -v -n
-
-nginx -s stop
-sleep 3
+if [ $? -eq 0 ]; then
+  echo "Certificate is already present and won't expire in the next week, skipping renewal"
+  cp /etc/letsencrypt/nginx.conf /etc/nginx/nginx.conf
+else
+  echo "Fetching certificate from LetsEncrypt"
+  nginx
+  sleep 5
+  certbot $staging_arg --nginx -d $MY_DOMAIN_NAME -d www.$MY_DOMAIN_NAME --text --agree-tos --email $EMAIL_ADDRESS -v -n
+  nginx -s stop
+  cp /etc/nginx/nginx.conf /etc/letsencrypt/nginx.conf
+  sleep 3
+fi
 
 nginx -g "daemon off;"
